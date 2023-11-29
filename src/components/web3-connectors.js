150523b295ac4aa9bdd0d47c666f654470615e7c
2036380 <charlesmuoka1@gmail.com>
2023-11-29 16:21:55 +0100

    another commit

--- src/components/web3-connectors.js
+++
@@ -1,7 +0,0 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-
